2ebf20ce6cc2bce0b6d3824e0f7f4cc37f39e324
lrubioh <l.rubioh@edu.uah.es>
2025-02-13 17:15:10 +0100

    MyPoint(parte 1)

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,35 @@
-import es.uah.matcomp.mp.e2.ejerciciosclases.e3.Account;
-import es.uah.matcomp.mp.e2.ejerciciosclases.e3.Customer;
+import es.uah.matcomp.mp.e2.ejerciciosclases.e4.MyPoint;
 public class Main {
     public static void main(String[] args) {
-        Customer c1 = new Customer(99, "Laura", 'f');
-        Customer c2 = new Customer(100, "Maria", 'f');
-        System.out.println(c1); ///customers tostring()
-        System.out.println(c2);
 
-        /// no hay set con lo que no puedo probar a establecer un nuevo
-        ///id o un nuevo nombre para un customer ya definido si se pudiese lo
-        /// implementaria de la forma :
-        /// c1.setId(valor del id nuevo)
+        /// pruebo todos los metodos para ver que funcionen correctamente
 
-        /// ya que si hay getters pruebo que funcionen bien estos lo que hacen
-        /// es devolverme los valores que ya tengo estabecidos en la persona
+        MyPoint p1 = new MyPoint(); //p1 =(0,0) por el MyPoint()
+        System.out.println(p1); //imprimo p1 con el String to String
+        //con los setters cambio las coord de p1 por las nuevas que estoy estableciendo
+        p1.setX(8);
+        p1.setY(6);
+        // lo imprimo usando la funcion getX y getY para probarlas tambien, la otra
+        //opcion es el string to string pero ese ya le he usado con lo que lo pruebo
+        //a traves de estos
+        System.out.println("x is: " + p1.getX()); // Test getters
+        System.out.println("y is: " + p1.getY());
+        //pruebo la funcion de setXY para establecer las coordenadas en una sola funcion
+        //y no en dos como he hecho arriba
+        p1.setXY(3, 0);
+        //pruebo la funcion getXY int[]
+        System.out.println(p1.getXY()[0]);
+        System.out.println(p1.getXY()[1]);
+        System.out.println(p1);
+        //creo un nuevo punto y lo imprimo por pantalla
+        MyPoint p2 = new MyPoint(0, 4);
+        System.out.println(p2);
 
-        System.out.println("id is:" + c1.getId());
-        System.out.println("name is:" + c1.getName());
-        System.out.println("Gender is:" + c1.getGender());
-
-
-        System.out.println("id is:" + c2.getId());
-        System.out.println("name is:" + c2.getName());
-        System.out.println("Gender is:" + c2.getGender());
-
-        /// Pruebo la parte del codigo de account
-
-        /// establezco dos cuentas de dos personas
-        Account a1 = new Account(99, c1);
-        Account a2 = new Account(100, c2);
-        System.out.println(a1);
-        System.out.println(a2);
-
-        /// Introduzco un balance a cada una de las cuentas
-        a1.setBalance(100);
-        System.out.println(a1);
-        a2.setBalance(200);
-        System.out.println(a2);
-
-        /// Imprimo el balance que he establecido arriba
-        System.out.println("balance is:" + a1.getBalance());
-        System.out.println("balance is:" + a2.getBalance());
-
-        ///deposit
-
-        a1.deposit(100);
-        a2.deposit(200);
-        System.out.println(a1);
-        System.out.println(a2);
-
-        ///withdraw
-        /// cuenta 1 tiene 200 y pago 100
-        a1.withdraw(100);
-        System.out.println("balance is:"+ a1.getBalance());
-        /// cuenta 2 tiene 400 y pago 300
-        a2.withdraw(300);
-        System.out.println("balance is:"+ a2.getBalance());
-        /// cuenta 1 tiene 100 y pago 50
-        a1.withdraw(50);
-        System.out.println("balance is:" + a1.getBalance());
-        /// busco el error
-        a1.withdraw(70);
-        System.out.println("balance is:" + a1.getBalance());
-
-
-
-
-
-
+        /// PRUEBO LOS METODOS DE LAS DISTANCIAS
+        System.out.println(p1.distance(p2)); // which version? ANOTHER
+        System.out.println(p2.distance(p1)); // which version? ANOTHER
+        System.out.println(p1.distance(5, 6)); // which version? INT X, INT Y
+        System.out.println(p1.distance()); // which version? DISTANCIA CON EL 0
 
     }
 
