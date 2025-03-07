dc854affc05e4f373159b3b5b5ad06f75a36b38f
lrubioh <l.rubioh@edu.uah.es>
2025-03-07 18:09:14 +0100

    Person, Student , Staff

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,38 @@
 import es.uah.matcomp.mp.e3.ejerciciosclases.e1.Cylinder;
+import es.uah.matcomp.mp.e3.ejerciciosclases.e2.Person;
+import es.uah.matcomp.mp.e3.ejerciciosclases.e2.Staff;
+import es.uah.matcomp.mp.e3.ejerciciosclases.e2.Student;
 
 
 public class Main {
-    public static void main (String[] args) {
-        // Declare and allocate a new instance of cylinder
-        // with default color, radius, and height
-        Cylinder c1 = new Cylinder();
-        System.out.println("Cylinder:"
-                + " radius=" + c1.getRadius()
-                + " height=" + c1.getHeight()
-                + " base area=" + c1.getArea()
-                + " volume=" + c1.getVolume());
+    public static void main(String[] args) {
+        //PARTE DE PERSON
+        Person persona1= new Person("Laura", "laura.rubio.hernan@gmail.com");
+        Person persona2= new Person("Adrian", "adrian.cano@gmail.com");
+        System.out.println(persona1.toString());
+        persona2.setAddress(persona1.getAddress());
+        System.out.println(persona2.toString());
 
-        // Declare and allocate a new instance of cylinder
-        // specifying height, with default color and radius
-        Cylinder c2 = new Cylinder(10.0);
-        System.out.println("Cylinder:"
-                + " radius=" + c2.getRadius()
-                + " height=" + c2.getHeight()
-                + " base area=" + c2.getArea()
-                + " volume=" + c2.getVolume());
+        //PARTE DE STUDENT
+        Student student1 = new Student("John", "john.cano@gmail.com");
+        Student student2 = new Student("Jane", "jane.cano@gmail.com", "science",2025,1000);
+        System.out.println(student1.toString());
+        student2.setProgram("letras");
+        System.out.println(student2.getProgram());
+        student2.setYear(2020);
+        System.out.println(student2.getYear());
+        student2.setFee(16);
+        System.out.println(student2.getFee());
 
-        // Declare and allocate a new instance of cylinder
-        // specifying radius and height, with default color
-        Cylinder c3 = new Cylinder(2.0, 10.0);
-        System.out.println("Cylinder:"
-                + " radius=" + c3.getRadius()
-                + " height=" + c3.getHeight()
-                + " base area=" + c3.getArea()
-                + " volume=" + c3.getVolume());
-    }
-
-
+        //PARTE DEL STAFF
+        Staff staff1 = new Staff("John", "john.cano@gmail.com","ellacuria",50);
+        Staff staff2 = new Staff("Jane", "jane.cano@gmail.com","quimica",70);
+        System.out.println(staff1.toString());
+        staff1.setSchool(staff2.getSchool());
+        System.out.println(staff1.getSchool());
+        staff2.setPay(273);
+        System.out.println(staff1.getPay());
+        System.out.println(staff2.toString());
+     }
 }
 
---
+++ src/es/uah/matcomp/mp/e3/ejerciciosclases/e2/Student.java
@@ -0,0 +1,42 @@
+package es.uah.matcomp.mp.e3.ejerciciosclases.e2;
+
+public class Student extends Person{
+    private String program;
+    private int year;
+    private double fee;
+    //me lo pone el programa
+    public Student(String name, String address) {
+        super(name, address);
+    }
+    public Student(String name, String address, String program, int year, double fee) {
+        super(name, address);
+        this.program = program;
+        this.year = year;
+        this.fee = fee;
+    }
+    public String getProgram() {
+        return program;
+    }
+    public void setProgram(String program) {
+        this.program = program;
+    }
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public double getFee() {
+        return fee;
+    }
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+    public String toString() {
+        return "Student"+ super.toString()
+                +"program=" + program + ", year=" + year + ", fee=" + fee
+                + "]";
+    }
+
+
+}
